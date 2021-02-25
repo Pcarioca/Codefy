1354134bc51b69b983f5964ed55d63d78ddcd533
Pcarioca <63117204+Pcarioca@users.noreply.github.com>
2021-02-25 12:02:00 +0200

    Nu merge js + am modificat design-ul
    executa functia vanish() in liip >:|

--- index.js
+++ index.js
@@ -1,98 +1,11 @@
+function vanish() {
+        console.log("STREG TOT!")
+        localStorage.clear();
+        location.reload();
+
+    }
 document.addEventListener('DOMContentLoaded', function () {
-
-    document.getElementById("input").onkeypress = tasta;
-    document.getElementsByClassName("minus").onkeypress = sterge;
-
-    if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
-    /* Daca utilizatorul nu a mai folosit platforma TO-do, atunci
-     *@getItem("nr") ar trebui sa nu existe deci sa initializeze i cu 0;
-     *altfel, ar trebui sa aiba o valoare sub forma de String si sa continue
-     *de unde a ramas cu numaratoarea.
-     */ { i = 0; }
-    else { i = parseInt(localStorage.getItem("nr"), 10); }
-
-    
-    console.log("prima data i=" + i);
-    console.log("parseInt(localStorage.getItem('nr'),10)=", + parseInt(localStorage.getItem("nr"), 10));
-    console.log("localStorage.getItem('nr')=" + localStorage.getItem("nr"));
-
-    //  !!  Restabileste TO-do-urile din localStorage.  !!  (functioneaza perfect)(sau lasa)
-
-
-    var count = parseInt(localStorage.getItem("nr"), 10);
-
-
-
-    var char2 = "";
-    var todo3 = "";
-    for (var k = 1; k <= count; k++) {
-        console.log(" in primul for k=" + k);
-        todo3 = localStorage.getItem(k.toString());
-        if (todo3 == null) continue;
-        console.log("localStorage.getItem(k.toString())=" + localStorage.getItem(k.toString()));
-        var para = document.createElement("p");
-        var node = document.createTextNode(todo3);
-        para.appendChild(node);
-        Pdiv=document.createElement("div");
-
-        var element = document.getElementById("div1");
-
-            var RemoveDIV=document.createElement("div");
-            element.appendChild(RemoveDIV);
-            
-            var RemoveBTN = document.createElement("button");
-            var minus = document.createTextNode("-");
-
-
-        element.appendChild(para);
-    
-        var unde = document.getElementById("remover");
-        para.setAttribute("id","button"+k.toString());
-
-        var br = document.createElement("br");
-        
-        br.setAttribute("id","br"+k.toString());
-        br.setAttribute("class","br");
-        
-
-            
-
-
-            var unde = document.getElementById("remover");
-            RemoveBTN.appendChild(minus);
-            unde.appendChild(RemoveBTN);
-            RemoveBTN.setAttribute("name",k.toString());
-            RemoveBTN.setAttribute("id","button"+k.toString());
-            RemoveBTN.setAttribute("class","minus btn-block btn "+k.toString()); 
-           
-            unde.appendChild(RemoveDIV);
-            RemoveDIV.appendChild(RemoveBTN);
-            
-            listuta=document.getElementById("div1");
-            div2=document.createElement("div");
-            listuta.appendChild(div2);
-            div2.appendChild(para);
-
-
-            
-        
-    }   
-       
-
-
-
-
-
-
-    var todo = [];
-    document.getElementById("buton").onclick = add;
-    document.getElementById("button").onclick = storage;
-    document.getElementById("vanish").onclick = vanish;
-
-
-
-
-    function add() {
+function add() {
         if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
             i = 1;
         else
@@ -113,12 +26,12 @@ document.addEventListener('DOMContentLoaded', function () {
             para.appendChild(node);
             var element = document.getElementById("div1");
             element.appendChild(para);
-            let stringI=i.toString();
-            para.setAttribute("id","button"+stringI);
-            var br = document.createElement("br");
-            
-            br.setAttribute("id","br"+stringI);
-            br.setAttribute("class","br"+stringI);
+            let stringI = i.toString();
+            para.setAttribute("id", "button" + stringI);
+            var br = document.createElement("div");
+
+            br.setAttribute("id", "br" + stringI);
+            br.setAttribute("class", "br" + stringI);
             element.appendChild(br);
 
 
@@ -130,11 +43,11 @@ document.addEventListener('DOMContentLoaded', function () {
             var unde = document.getElementById("remover");
             var div3 = document.getElementById("div1");
             RemoveBTN.appendChild(minus);
-            
-            RemoveBTN.setAttribute("name",k.toString());
-            RemoveBTN.setAttribute("class","minus btn-block btn "+i.toString());
-            
-            
+
+            RemoveBTN.setAttribute("name", k.toString());
+            RemoveBTN.setAttribute("class", "minus btn-block btn " + i.toString());
+
+
             i++;
             document.getElementById("input").value = "";
 
@@ -143,11 +56,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
             localStorage.setItem("nr", i.toString());//salveaza ultimul index introdus in vector, identificator "nr".        
 
-            div4=document.createElement("div");
+            div4 = document.createElement("div");
             div3.appendChild(div4);
             div3.appendChild(RemoveBTN);
 
-            
+
 
 
 
@@ -178,12 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     }
-    function vanish() {
-
-        localStorage.clear();
-        location.reload();
-
-    }
+    
     function tasta() {
 
         var char = event.which || event.keyCode;
@@ -193,19 +101,113 @@ document.addEventListener('DOMContentLoaded', function () {
     function sterge() {
 
         alert("Am intrat in functie");
-        var deSters=RemoveBTN.name;
-        var nrDeSters=parseInt(deSters,10);
-        
+        var deSters = RemoveBTN.name;
+        var nrDeSters = parseInt(deSters, 10);
 
-        
+
+
         var bll = localStorage.getItem("nr");
         bll--;
         localStorage.setItem("nr", bll);
         location.reload();
 
-            }
+    }
+    document.getElementById("input").onkeypress = tasta;
+    document.getElementsByClassName("minus").onkeypress = sterge;
+
+    if (localStorage.getItem("nr") == null || localStorage.getItem("nr") == undefined)
+    /* Daca utilizatorul nu a mai folosit platforma TO-do, atunci
+     *@getItem("nr") ar trebui sa nu existe deci sa initializeze i cu 0;
+     *altfel, ar trebui sa aiba o valoare sub forma de String si sa continue
+     *de unde a ramas cu numaratoarea.
+     */ { i = 0; }
+    else { i = parseInt(localStorage.getItem("nr"), 10); }
+
+
+    console.log("prima data i=" + i);
+    console.log("parseInt(localStorage.getItem('nr'),10)=", + parseInt(localStorage.getItem("nr"), 10));
+    console.log("localStorage.getItem('nr')=" + localStorage.getItem("nr"));
+
+    //  !!  Restabileste TO-do-urile din localStorage.  !!  (functioneaza perfect)(sau lasa)
+
+
+    var count = parseInt(localStorage.getItem("nr"), 10);
+
+
+
+    //var char2 = "";
+    //var todo3 = "";
+    for (var k = 1; k <= count; k++) {
+        console.log(" in primul for k=" + k);
+        todo3 = localStorage.getItem(k.toString());
+        if (todo3 == null) continue;
+        console.log("localStorage.getItem(k.toString())=" + localStorage.getItem(k.toString()));
+        var para = document.createElement("p");
+        var node = document.createTextNode(todo3);
+        para.appendChild(node);
+        Pdiv = document.createElement("div");
+
+        var element = document.getElementById("div1");
+
+        var RemoveDIV = document.createElement("div");
+        element.appendChild(RemoveDIV);
+
+        var RemoveBTN = document.createElement("button");
+        var minus = document.createTextNode("-");
+
+
+        element.appendChild(para);
+
+        var unde = document.getElementById("remover");
+        para.setAttribute("id", "button" + k.toString());
+
+        var br = document.createElement("br");
+
+        br.setAttribute("id", "br" + k.toString());
+        br.setAttribute("class", "br");
+
+
+
+
+
+        var unde = document.getElementById("remover");
+        RemoveBTN.appendChild(minus);
+        unde.appendChild(RemoveBTN);
+        RemoveBTN.setAttribute("name", k.toString());
+        RemoveBTN.setAttribute("id", "button" + k.toString());
+        RemoveBTN.setAttribute("class", "minus btn-block btn " + k.toString());
+
+        unde.appendChild(RemoveDIV);
+        RemoveDIV.appendChild(RemoveBTN);
+
+        listuta = document.getElementById("div1");
+        div2 = document.createElement("div");
+        listuta.appendChild(div2);
+        div2.appendChild(para);
+
+
+
+
+    }
+
+
+
+
+
+
+
+    var todo = [];
+    document.getElementById("buton").onclick = add;
+    document.getElementById("button").onclick = storage;
+    document.getElementById("vanish").onclick = vanish;
+
+
+
+
     
-    
+
+
 
 
 });
+document.getElementById("vanish").addEventListener("click",vanish());
